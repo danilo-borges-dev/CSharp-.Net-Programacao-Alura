6fadb705bb099ad3ff0c6822a54f0744581644ba
danilo-borges-dv <danilosilva2017@hotmail.com>
2021-12-13 06:18:24 -0300

    Primeiro Projeto em CSharp

---
+++ _01_CSharp_PrimeirosPassos/_01_PrimeiroProjeto/_01_PrimeiroProjeto/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _01_PrimeiroProjeto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
