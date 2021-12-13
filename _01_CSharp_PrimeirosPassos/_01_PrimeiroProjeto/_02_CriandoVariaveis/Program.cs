112193ed73c74b5d4cc5c9d02cabb1c3ecb344fc
danilo-borges-dv <danilosilva2017@hotmail.com>
2021-12-13 06:57:16 -0300

    Criando as primeiras Veriaveis em CSharp

---
+++ _01_CSharp_PrimeirosPassos/_01_PrimeiroProjeto/_02_CriandoVariaveis/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _02_CriandoVariaveis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Criando Projeto 2 - Criando Variáveis.");
+
+            int idade = 28;
+
+            Console.WriteLine($"Minha Idade é {idade}");
+
+            Console.WriteLine("Tecle Enter para Sair!");
+            Console.ReadLine();
+        }
+    }
+}
