0dc11f728356559f57ca7068bd8a6dde8a680b28
danilo-borges-dv <danilosilva2017@hotmail.com>
2021-12-15 06:21:50 -0300

    Pratica Basica com Caracteres e Textos

---
+++ _01_CSharp_PrimeirosPassos/_05_CaracteresETextos/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace _05_CaracteresETextos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando o Projeto 5 - Caracteres e Textos");
+
+            char primeiraLetraNome = 'D';
+            Console.WriteLine(primeiraLetraNome);
+
+            Console.WriteLine();
+
+            // Conversão "Cast" explícita de um valor inteiro para char.    
+            primeiraLetraNome = (char)65;  // A conversão resulta em um valor existente na tabela ascii                  
+            Console.WriteLine(primeiraLetraNome);
+
+            Console.WriteLine();
+
+            string titulo = "Alura Cursos de Tecnologia " + 2021 + "."; // Concatenação de Caracteres
+            Console.WriteLine(titulo);
+
+            Console.WriteLine();
+
+            Console.WriteLine(titulo[0]); // String é um Vetor de Caractere, então, é possível acessar as posições do Vetor de Caracateres e exibir o valor
+
+            Console.WriteLine();
+
+            string mensagem = @"--------------------------------Primeira linha
+--------------------------------Segunda Linha
+--------------------------------Terceira Linha";
+
+            Console.WriteLine(mensagem);
+        }
+    }
+}
