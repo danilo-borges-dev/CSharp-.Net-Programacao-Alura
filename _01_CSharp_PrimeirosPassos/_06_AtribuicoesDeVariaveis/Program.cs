b045280a29c64dc22891b0702ee235a8c1c382ca
danilo-borges-dv <danilosilva2017@hotmail.com>
2021-12-15 06:42:07 -0300

    Trabalhando com o Condicional IF

---
+++ _01_CSharp_PrimeirosPassos/_06_AtribuicoesDeVariaveis/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _06_AtribuicoesDeVariaveis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando o Projeto 6");
+            int idade = 28;
+            int idadeGustavo = idade;
+
+            Console.WriteLine(idadeGustavo);
+        }
+    }
+}
---
+++ _01_CSharp_PrimeirosPassos/_07_Condicionais/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _07_Condicionais
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando Projeto 7 - Condicionais");
+
+            int idadeJoao = 16;
+            int quantidadePessoas = 1;
+
+            if (idadeJoao >= 18)
+            {
+                Console.WriteLine("Maior de Idade! Pode Entrar!");
+            }
+            else
+            {
+                if (quantidadePessoas > 1)
+                {
+                    Console.WriteLine("João não é maior de idade mas está acompanhado! Pode Entrar!");
+                }
+                else
+                {
+                    Console.WriteLine("Menor de idade! Não Pode Entrar!");
+                }
+            }
+        }
+    }
+}
