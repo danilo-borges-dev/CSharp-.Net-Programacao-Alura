12034012c5f90e625f1716d4568cc18050133851
danilo-borges-dv <danilosilva2017@hotmail.com>
2021-12-15 06:51:14 -0300

    Melhorando o codigo com o operador ou ||

---
+++ _01_CSharp_PrimeirosPassos/_08_Condicionais2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _08_Condicionais2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando Projeto 7 - Condicionais");
+
+            int idadeJoao = 16;
+            int quantidadePessoas = 2;
+
+            if (idadeJoao > 18 || quantidadePessoas > 1)
+            {
+                Console.WriteLine("João não é maior de idade mas está acompanhado! Pode Entrar!");
+            }
+            else
+            {
+                if (idadeJoao >= 18)
+                {
+                    Console.WriteLine("João é maior de idade! Pode Entrar!");
+                }
+                else
+                {
+                    Console.WriteLine("Menor de idade! Não Pode Entrar!");
+                }
+            }
+        }
+    }
+}
+
