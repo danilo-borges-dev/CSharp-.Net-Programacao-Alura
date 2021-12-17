72bef3bd453b752d2119e739de86f08dccc9a07d
danilo-borges-dv <danilosilva2017@hotmail.com>
2021-12-17 06:38:14 -0300

    Melhorei o Codigo Apos Adicionar o Laco de Controle For, tambem fiz algumas correcoes

--- _01_CSharp_PrimeirosPassos/_11_CalculaPoupanca2/Program.cs
+++ _01_CSharp_PrimeirosPassos/_11_CalculaPoupanca2/Program.cs
@@ -1,24 +1,22 @@
 ﻿using System;
 using System.Globalization;
 
-namespace _10_CalculaPoupanca
+namespace _11_CalculaPoupanca2
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Executando Projeto 10 - Calcula Poupança");
+            Console.WriteLine("Executando Projeto 11 - Calcula Poupança");
 
             double valorInvestido = 1_000;
-            int i = 1;
 
             Console.WriteLine($"Valor Investido R${valorInvestido.ToString("F2", CultureInfo.InvariantCulture)}");
 
-            while(i <= 12)
+            for (int i = 1; i <= 12; i++)
             {
-                valorInvestido += (valorInvestido * 0.0036); // 0.36% = 0.0036
+                valorInvestido *= 1.0036; // valorInvestido += 1_000 * 1.0036 - Em Multiplicação refere que qualquer número multiplicado por "1" mantém a sua identidade (fica igual)
                 Console.WriteLine($"Valor Investido em {i} Mês(s) R${valorInvestido.ToString("F2", CultureInfo.InvariantCulture)}");
-                i++;
             }
         }
     }
