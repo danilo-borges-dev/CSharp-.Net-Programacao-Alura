f824182250b881b7b6a486a0eb7bb3af43100dc5
danilo-borges-dv <danilosilva2017@hotmail.com>
2021-12-20 06:49:55 -0300

    Primeira Classe Criada no C Sharp

---
+++ _02_Introducao_Orientacao_Objetos/_01_ByteBank/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _01_ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente contaDaBruna = new ContaCorrente();
+
+            contaDaBruna.Nome = "Bruna";
+            contaDaBruna.Agencia = 123;
+            contaDaBruna.Numero = 555;
+            contaDaBruna.Saldo = 100;
+
+            
+        }
+    }
+}
