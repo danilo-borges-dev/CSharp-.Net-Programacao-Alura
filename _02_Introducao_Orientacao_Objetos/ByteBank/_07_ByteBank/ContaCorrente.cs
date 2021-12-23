478d6ef628f9bbdeba9e7631572b5f898cbf93d5
danilo-borges-dv <danilosilva2017@hotmail.com>
2021-12-23 20:15:07 -0300

    Adicionado Projeto 07 ByteBank

---
+++ _02_Introducao_Orientacao_Objetos/ByteBank/_07_ByteBank/ContaCorrente.cs
@@ -0,0 +1,46 @@
+﻿namespace _07_ByteBank
+{
+    internal class ContaCorrente
+    {
+        public Cliente Titular = new Cliente();
+        public int Agencia { get; }
+        public int Numero { get; }
+        private double _saldo;
+        public bool Sacar(double valor)
+        {
+            if (valor > _saldo)  // Existe um retorno, portanto é comum chamar de Função
+            {
+                Console.WriteLine($"_saldo Insuficiente - _saldo Atual R${_saldo.ToString("F2")}");
+                return false;
+            }
+            _saldo -= valor;
+            Console.WriteLine($"Saque Realizado com Sucesso - Novo _saldo em Conta R${_saldo.ToString("F2")}");
+            return true;
+        }
+        public void Depositar(double valor)  // Método
+        {
+            if (valor < 0)
+            {
+                Console.WriteLine("Valor Inválido para Depósito!");
+            }
+            _saldo += valor;
+            Console.WriteLine($"Depósito  realizado com Sucesso! - Novo _saldo em Conta R${_saldo}");
+        }
+        public double GetSaldo()
+        {
+            return _saldo;
+        }
+        public bool Transferir(double valor, ContaCorrente contaDestino)
+        {
+            if (valor > _saldo)
+            {
+                Console.WriteLine($"_saldo Insuficiente para Transferir - _saldo Atual em Conta R${_saldo}");
+                return false;
+            }
+            _saldo -= valor;
+            contaDestino.Depositar(valor);
+            Console.WriteLine($"Transferência Realizada com Sucesso! - Titular {Titular} - _saldo Atual em Conta R${_saldo}");
+            return true;
+        }
+    }
+}
---
+++ _02_Introducao_Orientacao_Objetos/ByteBank/_07_ByteBank/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace _07_ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente novaConta = new();
+
+            // novaConta.Titular = new Cliente();
+
+            novaConta.Titular.Nome = "Danilo";
+            novaConta.Titular.Proficao = "Engenheiro de Software";
+
+            novaConta.Depositar(100_000_000_000);
+
+            Console.WriteLine(novaConta.Titular.Nome);
+            Console.WriteLine(novaConta.Titular.Proficao);
+
+            Console.WriteLine();
+
+            ContaCorrente novaConta2 = new();
+
+            novaConta2.Titular.Nome = "Marcos";
+            novaConta2.Depositar(1_000_000);
+            novaConta2.Titular.Proficao = "Empresário";
+
+            Console.WriteLine(novaConta2.Titular.Nome);
+            Console.WriteLine(novaConta2.Titular.Proficao);
+
+            Console.WriteLine();
+
+            Console.WriteLine(novaConta2.GetSaldo());
+        }
+    }
+}
