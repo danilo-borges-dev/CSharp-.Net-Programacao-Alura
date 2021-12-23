67912c4a91692e9bc36910a6903c2321f51fe248
danilo-borges-dv <danilosilva2017@hotmail.com>
2021-12-23 19:52:34 -0300

    Adicionado Projeto 06 ByteBank

---
+++ _02_Introducao_Orientacao_Objetos/ByteBank/_06_ByteBank/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace _06_ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente contaDoJose = new();
+
+            contaDoJose.Titular = new Cliente();  // O Objeto contaDoJose possui uma Propriedade (variavel) que esta apotando (referencia) para as Propriedades da Classe Cliente.
+
+            contaDoJose.Titular.Nome = "José";
+            contaDoJose.Titular.Proficao = "Programador C#";
+
+            Console.WriteLine(contaDoJose.Titular.Nome);
+            Console.WriteLine(contaDoJose.Titular.Proficao);
+        }
+    }
+}
