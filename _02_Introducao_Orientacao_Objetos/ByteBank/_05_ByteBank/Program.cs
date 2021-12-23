bb3e967b73a27a57ca7b9f38956c88d9b520b0e7
danilo-borges-dv <danilosilva2017@hotmail.com>
2021-12-23 19:40:22 -0300

    Trabalhando com Composicao de Objeto

--- _02_Introducao_Orientacao_Objetos/ByteBank/_05_ByteBank/Program.cs
+++ _02_Introducao_Orientacao_Objetos/ByteBank/_05_ByteBank/Program.cs
@@ -4,15 +4,23 @@
     {
         static void Main(string[] args)
         {
-            Cliente novoCliente = new();
+            Cliente novoCliente = new();  // Variavel novoCliente, referencia para o Objeto Cliente
 
             novoCliente.Nome = "Gabriela";
 
             ContaCorrente contaDaGabriela = new();
 
-            contaDaGabriela.Titular = novoCliente;
+            contaDaGabriela.Titular = novoCliente;  // Propriedade Titular recebe a Referencia para o Objeto Cliente "novoCliente"
 
             Console.WriteLine(contaDaGabriela.Titular.Nome);
+
+            Console.WriteLine();
+
+            contaDaGabriela.Titular.Nome = "João";
+            contaDaGabriela.Titular.Proficao = "Desenvolvedor C#"; // Atraves da variavel de Referencia Titular podemos acessar propriedades da Classe Cliente
+
+            Console.WriteLine(contaDaGabriela.Titular.Nome);
+            Console.WriteLine(contaDaGabriela.Titular.Proficao);
 
             Console.WriteLine();
         }
